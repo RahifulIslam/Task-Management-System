fbe1d497b5f4496a17275722b08f28b6e424ae79
Rahiful Islam <rahifulislam@gmail.com>
2024-02-07 18:38:36 +0600

    completed setup

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./service.json');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+
+mongoose.connect(config.MONGODB_SERVER)
+         .then(()=> console.log("Connected to MongoDB!"))
+         .catch(err => {
+            console.log("MongoDB connection failed", err.message)
+         });
+
+const port = config.PORT || 6000;
+app.listen(port, (err)=>{
+   if(err){
+      console.log("Server failed to start:", err.message);
+   }
+   console.log(`Listening on port ${port}`);
+});
